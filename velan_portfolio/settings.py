04524f025c0dd91feb859c9d13071ee61e3bee22
velan-07 <velan00777@gmail.com>
2025-01-10 21:00:34 +0530

    templates dir change

--- velan_portfolio/settings.py
+++ velan_portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%hfo7@6c(ptnkacx#+4oz9+nq+x=qqqf536=1oq4z6=+v5wu@a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [ '*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
